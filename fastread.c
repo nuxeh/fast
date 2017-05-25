2023563839f8ae2b37aee56c893b96d8cccf2c93
Edward Cragg <edward.cragg@codethink.co.uk>
2017-05-25 11:31:51 +0100

    fastread: tidy up

--- fastread.c
+++ fastread.c
@@ -2,28 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-void pgm_write(const char *filename,
-	       int width, int height, unsigned char *buf)
-{
-	FILE *f = fopen(filename,"wb");
-	if(f) {
-		fprintf(f,"P5\n%d %d\n255\n",width,height);
-		fwrite(buf,width,height,f);
-		fclose(f);
-	}
-}
+#include "pgm.x"
 
-void putpixel(unsigned char *img, int x, int y)
-{
-	static int count = 0;
-
-	img[x + y * 100] ^= 255;
-
-	count++;
-	//printf("count: %d\n", count);
-}
-
-struct xy {
+struct quirc_point {
 	int x;
 	int y;
 };
@@ -31,10 +12,10 @@ struct xy {
 struct octant {
 	int r;
 	int n;
-	struct xy *ds[];
+	struct quirc_point *ds[];
 };
 
-static inline void set_point(struct xy *p, int m, int i, int x, int y)
+static inline void set_point(struct quirc_point *p, int m, int i, int x, int y)
 {
 	if (i >= m)
 		return;
@@ -48,32 +29,13 @@ void circle(unsigned char *img, int xc, int yc, int r)
 	int d, x, y, p, i, l;
 
 	int ii = 0;
-	struct xy *c_pixels;
+	struct quirc_point *c_pixels;
 
 	d = 3 - 2 * r;
 	x = 0;
 	y = r;
 	while(x <= y)
 	{
-		#if 1
-		putpixel(img, xc + x, yc + y);
-		putpixel(img, xc - y, yc - x);
-		putpixel(img, xc + y, yc - x);
-		putpixel(img, xc - y, yc + x);
-		putpixel(img, xc + y, yc + x);
-		putpixel(img, xc - x, yc - y);
-		putpixel(img, xc + x, yc - y);
-		putpixel(img, xc - x, yc + y);
-		#endif
-		#if 0
-		char out_str[20];
-		sprintf(out_str, "/tmp/bcirc2__%03d.pgm\0", ii++);
-		printf("%s\n", out_str);
-		pgm_write(out_str, 100, 100, img);
-		#endif
-
-		printf("x: %d y: %d\n", x, y);
-
 		if(d <= 0) {
 			d = d + 4 * x + 6;
 		} else {
@@ -85,8 +47,8 @@ void circle(unsigned char *img, int xc, int yc, int r)
 
 	p = x;
 	l = (p-1) * 8;
-	c_pixels = malloc(l * sizeof(struct xy));
-	memset(c_pixels, 0, l * sizeof(struct xy));
+	c_pixels = malloc(l * sizeof(struct quirc_point));
+	memset(c_pixels, 0, l * sizeof(struct quirc_point));
 
 	printf("x: %d\n", p);
 
@@ -95,7 +57,6 @@ void circle(unsigned char *img, int xc, int yc, int r)
 	y = r;
 	while (x <= y)
 	{
-		printf("x: %d y: %d\n", x, y);
 		set_point(c_pixels, l, x,	  +x, -y);
 		set_point(c_pixels, l, (2*p)-x-2, +y, -x);
 		set_point(c_pixels, l, (2*p)+x-2, +y, +x);
@@ -105,7 +66,6 @@ void circle(unsigned char *img, int xc, int yc, int r)
 		set_point(c_pixels, l, (6*p)+x-6, -y, -x);
 		set_point(c_pixels, l, (8*p)-x-8, -x, -y);
 
-
 		if(d <= 0) {
 			d = d + 4 * x + 6;
 		} else {
@@ -113,46 +73,24 @@ void circle(unsigned char *img, int xc, int yc, int r)
 			y = y - 1;
 		}
 		x++;
-
-
-		for (i = 0; i < (p-1) * 8; i++) {
-			if (c_pixels[i].x == 0 && c_pixels[i].y == 0)
-				printf("_");
-			else
-				printf("#");
-		}
-		printf("\n");
 	}
-	#if 1
-	//unsigned char *img2 = malloc(100 * 100);
-	//memset(img2, 255, 1000 * 1000);
-	for (i = 0; i < (p-1) * 8; i++) {
-		putpixel(img, xc + c_pixels[i].x + 10, yc + c_pixels[i].y);
-		#if 1
-		char out_str[20];
-		sprintf(out_str, "/tmp/bcirc2_%03d.pgm\0", ii++);
-		printf("%s\n", out_str);
-		pgm_write(out_str, 100, 100, img);
-		#endif
-	}
-	//free(img2);
-	#endif
 	free(c_pixels);
 }
 
 int main(void)
 {
-	unsigned char *img = malloc(1000 * 1000);
-	memset(img, 255, 1000 * 1000);
+	unsigned char *imgc = malloc(1000 * 1000);
+	memset(imgc, 255, 1000 * 1000);
 
-//	circle(img, 500, 500, 100);
-//	circle(img, 500, 500, 20);
-//	circle(img, 500, 500, 10);
-//	circle(img, 500, 500, 5);
-//	circle(img, 500, 500, 2);
-//	circle(img, 500, 500, 3);
-	circle(img, 25, 75, 17);
+	circle(imgc, 50, 50, 15);
 
-	pgm_write("/tmp/bcirc.pgm", 100, 100, img);
+	pgm_write("/tmp/bcirc.pgm", 100, 100, imgc);
+
+
+	unsigned char *img;
+	int w, h;
+
+	pgm_write("/tmp/1.pgm", w, h, img);
+
 	free(img);
 }
